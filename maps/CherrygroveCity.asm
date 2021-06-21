b110c8c64ef196948688bde74ba8f8f83c7a86b4
U-LAPTOP-J49EUBLE\willy <willyfudgehj@gmail.com>
2021-06-20 22:19:35 -0500

    all Guide Gent text

--- maps/CherrygroveCity.asm
+++ maps/CherrygroveCity.asm
@@ -290,7 +290,6 @@ GuideGentMovement4:
 	step RIGHT
 	step RIGHT
 	step RIGHT
-	step RIGHT
 	turn_head UP
 	step_end
 
@@ -317,7 +316,6 @@ GuideGentMovement5:
 
 GuideGentMovement6:
 	step UP
-	step LEFT
 	step UP
 	step_end
 
@@ -351,78 +349,108 @@ CherrygroveCity_RivalExitsStageLeft:
 	step_end
 
 GuideGentIntroText:
-	text "You're a rookie"
-	line "trainer, aren't"
-	cont "you? I can tell!"
+	text "Ah, another"
+	line "rookie, eh?"
+	
+	para "Haha!"
+	line "Don't sweat it!"
 
-	para "That's OK! Every-"
-	line "one is a rookie"
-	cont "at some point!"
+	para "Everyone starts"
+	line "out as a rookie,"
+	cont "after all!"
 
-	para "If you'd like, I"
-	line "can teach you a"
-	cont "few things."
+	para "Anyways, want me"
+	line "to show you the"
+	cont "ropes? They don't"
+	
+	para "call me the GUIDE"
+	line "GENT for no"
+	cont "reason!"
 	done
 
 GuideGentTourText1:
-	text "OK, then!"
-	line "Follow me!"
+	text "Splendid!"
+	line "Come with me!"
 	done
 
 GuideGentPokecenterText:
-	text "This is a #MON"
-	line "CENTER. They heal"
-
-	para "your #MON in no"
-	line "time at all."
-
-	para "You'll be relying"
-	line "on them a lot, so"
-
-	para "you better learn"
-	line "about them."
+	text "This here is the"
+	line "#MON CENTER,"
+	cont "as you might have"
+	
+	para "guessed. For no"
+	line "charge at all,"
+	cont "the nurse inside"
+	
+	para "will heal your"
+	line "#MON back to"
+	cont "normal for free!"
+	
+	para "It's so wonderful!"
 	done
 
 GuideGentMartText:
-	text "This is a #MON"
-	line "MART."
+	text "And this is the"
+	line "# MART."
 
-	para "They sell BALLS"
-	line "for catching wild"
-
-	para "#MON and other"
-	line "useful items."
+	para "They have all"
+	line "sorts of useful"
+	cont "items for your" 
+	
+	para "journey…for a"
+	line "price. They're"
+	cont "slightly less"
+	
+	para "wonderful than the"
+	line "#MON CENTER."
 	done
 
 GuideGentRoute30Text:
-	text "ROUTE 30 is out"
-	line "this way."
+	text "Umm…yeah. That"
+	line "large building"
 
-	para "Trainers will be"
-	line "battling their"
+	para "over there is"
+	line "something that"
+	cont "I'd like to avoid."
 
-	para "prized #MON"
-	line "there."
+	para "Things get so"
+    line "rough in there"
+    cont "that #MON are"
+	
+	para "sent flying out"
+	line "of it! It's insane!"
 	done
 
 GuideGentSeaText:
-	text "This is the sea,"
-	line "as you can see."
-
-	para "Some #MON are"
-	line "found only in"
-	cont "water."
+	text "Ah, the" 
+	line "CHERRYGROVE CITY"
+	cont "GYM! GYMs are"
+	
+	para "where GYM LEADERS"
+	line "are, of course!"
+	cont "And you won't get"
+	
+	para "very far as a"
+	line "trainer if you"
+	cont "don't defeat the"
+	
+	para "GYM LEADERS!"
 	done
 
 GuideGentGiftText:
-	text "Here…"
-
-	para "It's my house!"
-	line "Thanks for your"
-	cont "company."
-
-	para "Let me give you a"
-	line "small gift."
+	text "And this here…is"
+	line "my house! Haha!"
+	cont "You've kept up"
+	
+	para "with me quite"
+	line "well! Most of the"
+	cont "time, people are"
+	
+	para "straggling behind"
+	line "me in these tours!"
+	cont "Here, have a small"
+	
+	para "gift as well."
 	done
 
 GotMapCardText:
@@ -431,20 +459,30 @@ GotMapCardText:
 	done
 
 GuideGentPokegearText:
-	text "#GEAR becomes"
-	line "more useful as you"
-	cont "add CARDS."
-
-	para "I wish you luck on"
-	line "your journey!"
+	text "Ah, the #GEAR!"
+	line "Such a lovely"
+	cont "tool!" 
+	
+	para "It seems you only"
+	line "have one more CARD"
+	cont "necessary to"
+	
+	para "complete yours!"
+	line "Best of luck"
+	cont "finding it!"	
 	done
 
 GuideGentNoText:
-	text "Oh… It's something"
-	line "I enjoy doing…"
-
-	para "Fine. Come see me"
-	line "when you like."
+	text "Oh…but it's"
+	line "basically my" 
+	cont "passion to show"
+	
+	para "people around"
+	line "town…and the only"
+	cont "thing I can"
+	
+	para "contribute to"
+	line "society anymore…"
 	done
 
 CherrygroveRivalText_Seen:
