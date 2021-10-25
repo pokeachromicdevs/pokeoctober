e9394adfd7e97956bc1feb13f299001f6e737ef8
U-LAPTOP-J49EUBLE\willy <willyfudgehj@gmail.com>
2021-10-25 00:36:25 -0500

    added Youngster Joey inside Cherrygrove Woods

--- maps/Route30.asm
+++ maps/Route30.asm
@@ -14,177 +14,10 @@ Route30_MapScripts:
 	db 0 ; scene scripts
 
 	db 0 ; callbacks
-
-YoungsterJoey_ImportantBattleScript:
-	waitsfx
-	playmusic MUSIC_JOHTO_TRAINER_BATTLE
-	opentext
-	writetext Text_UseTackle
-	pause 30
-	closetext
-	playsound SFX_TACKLE
-	applymovement ROUTE30_MONSTER2, Route30_JoeysRattataAttacksMovement
-	opentext
-	faceplayer
-	writetext Text_ThisIsABigBattle
-	waitbutton
-	turnobject ROUTE30_YOUNGSTER1, UP
-	closetext
-	playsound SFX_TACKLE
-	applymovement ROUTE30_MONSTER1, Route30_MikeysRattataAttacksMovement
-	special RestartMapMusic
-	end
-
-TrainerYoungsterJoey:
-	trainer YOUNGSTER, JOEY1, EVENT_BEAT_YOUNGSTER_JOEY, YoungsterJoey1SeenText, YoungsterJoey1BeatenText, 0, .Script
-
-.Script:
-	loadvar VAR_CALLERID, PHONE_YOUNGSTER_JOEY
-	endifjustbattled
-	opentext
-	checkflag ENGINE_JOEY
-	iftrue .Rematch
-	checkcellnum PHONE_YOUNGSTER_JOEY
-	iftrue .NumberAccepted
-	checkevent EVENT_JOEY_ASKED_FOR_PHONE_NUMBER
-	iftrue .AskAgain
-	writetext YoungsterJoey1AfterText
-	buttonsound
-	setevent EVENT_JOEY_ASKED_FOR_PHONE_NUMBER
-	scall .AskNumber1
-	sjump .RequestNumber
-
-.AskAgain:
-	scall .AskNumber2
-.RequestNumber:
-	askforphonenumber PHONE_YOUNGSTER_JOEY
-	ifequal PHONE_CONTACTS_FULL, .PhoneFull
-	ifequal PHONE_CONTACT_REFUSED, .NumberDeclined
-	gettrainername STRING_BUFFER_3, YOUNGSTER, JOEY1
-	scall .RegisteredNumber
-	sjump .NumberAccepted
-
-.Rematch:
-	scall .RematchStd
-	winlosstext YoungsterJoey1BeatenText, 0
-	readmem wJoeyFightCount
-	ifequal 4, .Fight4
-	ifequal 3, .Fight3
-	ifequal 2, .Fight2
-	ifequal 1, .Fight1
-	ifequal 0, .LoadFight0
-.Fight4:
-	checkevent EVENT_BEAT_ELITE_FOUR
-	iftrue .LoadFight4
-.Fight3:
-	checkevent EVENT_CLEARED_RADIO_TOWER
-	iftrue .LoadFight3
-.Fight2:
-	checkflag ENGINE_FLYPOINT_OLIVINE
-	iftrue .LoadFight2
-.Fight1:
-	checkflag ENGINE_FLYPOINT_GOLDENROD
-	iftrue .LoadFight1
-.LoadFight0:
-	loadtrainer YOUNGSTER, JOEY1
-	startbattle
-	reloadmapafterbattle
-	loadmem wJoeyFightCount, 1
-	clearflag ENGINE_JOEY
-	end
-
-.LoadFight1:
-	loadtrainer YOUNGSTER, JOEY2
-	startbattle
-	reloadmapafterbattle
-	loadmem wJoeyFightCount, 2
-	clearflag ENGINE_JOEY
-	end
-
-.LoadFight2:
-	loadtrainer YOUNGSTER, JOEY3
-	startbattle
-	reloadmapafterbattle
-	loadmem wJoeyFightCount, 3
-	clearflag ENGINE_JOEY
-	end
-
-.LoadFight3:
-	loadtrainer YOUNGSTER, JOEY4
-	startbattle
-	reloadmapafterbattle
-	loadmem wJoeyFightCount, 4
-	clearflag ENGINE_JOEY
-	end
-
-.LoadFight4:
-	loadtrainer YOUNGSTER, JOEY5
-	startbattle
-	reloadmapafterbattle
-	clearflag ENGINE_JOEY
-	checkevent EVENT_JOEY_HP_UP
-	iftrue .GiveHPUp
-	checkevent EVENT_GOT_HP_UP_FROM_JOEY
-	iftrue .done
-	scall .RematchGift
-	verbosegiveitem HP_UP
-	iffalse .PackFull
-	setevent EVENT_GOT_HP_UP_FROM_JOEY
-	sjump .NumberAccepted
-
-.done
-	end
-
-.GiveHPUp:
-	opentext
-	writetext YoungsterJoeyText_GiveHPUpAfterBattle
-	waitbutton
-	verbosegiveitem HP_UP
-	iffalse .PackFull
-	clearevent EVENT_JOEY_HP_UP
-	setevent EVENT_GOT_HP_UP_FROM_JOEY
-	sjump .NumberAccepted
-
-.AskNumber1:
-	jumpstd asknumber1m
-	end
-
-.AskNumber2:
-	jumpstd asknumber2m
-	end
-
-.RegisteredNumber:
-	jumpstd registerednumberm
-	end
-
-.NumberAccepted:
-	jumpstd numberacceptedm
-	end
-
-.NumberDeclined:
-	jumpstd numberdeclinedm
-	end
-
-.PhoneFull:
-	jumpstd phonefullm
-	end
-
-.RematchStd:
-	jumpstd rematchm
-	end
-
-.PackFull:
-	setevent EVENT_JOEY_HP_UP
-	jumpstd packfullm
-	end
-
-.RematchGift:
-	jumpstd rematchgiftm
-	end
-
-TrainerYoungsterMikey:
+	
+TrainerYoungsterMikey
 	trainer YOUNGSTER, MIKEY, EVENT_BEAT_YOUNGSTER_MIKEY, YoungsterMikeySeenText, YoungsterMikeyBeatenText, 0, .Script
-
+	
 .Script:
 	endifjustbattled
 	opentext
@@ -269,33 +102,6 @@ Text_ThisIsABigBattle:
 	text "What? This is a"
 	line "big battle!"
 	cont "Leave me alone!"
-	done
-
-YoungsterJoey1SeenText:
-	text "I just lost, so"
-	line "I'm trying to find"
-	cont "more #MON."
-
-	para "Wait! You look"
-	line "weak! Come on,"
-	cont "let's battle!"
-	done
-
-YoungsterJoey1BeatenText:
-	text "Ack! I lost again!"
-	line "Doggone it!"
-	done
-
-YoungsterJoey1AfterText:
-	text "Do I have to have"
-	line "more #MON in"
-
-	para "order to battle"
-	line "better?"
-
-	para "No! I'm sticking"
-	line "with this one no"
-	cont "matter what!"
 	done
 
 YoungsterMikeySeenText:
@@ -386,21 +192,6 @@ Route30TrainerTipsText:
 	cont "wild #MON!"
 	done
 
-YoungsterJoeyText_GiveHPUpAfterBattle:
-	text "I lost again…"
-	line "Gee, you're tough!"
-
-	para "Oh yeah, I almost"
-	line "forgot that I had"
-	cont "to give you this."
-
-	para "Use it to get even"
-	line "tougher, OK?"
-
-	para "I'm going to get"
-	line "tougher too."
-	done
-
 Route30_MapEvents:
 	db 0, 0 ; filler
 
@@ -414,8 +205,7 @@ Route30_MapEvents:
 	bg_event  1,  9, BGEVENT_READ, Route30TrainerTips
 	bg_event  4, 27, BGEVENT_ITEM, Route30HiddenPotion
 
-	db 8 ; object events
-	object_event  8, 22, SPRITE_YOUNGSTER, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_TRAINER, 3, TrainerYoungsterJoey, EVENT_ROUTE_30_YOUNGSTER_JOEY
+	db 7 ; object events
 	object_event  9, 17, SPRITE_YOUNGSTER, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_TRAINER, 1, TrainerYoungsterMikey, -1
 	object_event 12,  7, SPRITE_YOUNGSTER, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_TRAINER, 3, TrainerYoungsterMax, -1
 	object_event  6,  4, SPRITE_YOUNGSTER, SPRITEMOVEDATA_WALK_LEFT_RIGHT, 1, 0, -1, -1, PAL_NPC_RED, OBJECTTYPE_SCRIPT, 0, Route30YoungsterScript, -1
