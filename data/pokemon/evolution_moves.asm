e34aab0df7fec7df940be66d3d982a8d06554311
U-LAPTOP-J49EUBLE\willy <willyfudgehj@gmail.com>
2021-12-26 19:19:50 -0600

    Shadow Johto starter evo moves

--- data/pokemon/evolution_moves.asm
+++ data/pokemon/evolution_moves.asm
@@ -153,10 +153,10 @@ EvolutionMoves::
 	db PETAL_DANCE  ; MEGANIUM
 	db NO_MOVE      ; CYNDAQUIL
 	db NO_MOVE      ; QUILAVA
-	db NO_MOVE      ; TYPHLOSION
+	db FIRE_PUNCH   ; TYPHLOSION
 	db NO_MOVE      ; TOTODILE
 	db NO_MOVE      ; CROCONAW
-	db NO_MOVE      ; FERALIGATR
+	db WATERFALL    ; FERALIGATR
 	db NO_MOVE      ; BAYLEEF
 	db NO_MOVE      ; SENTRET
 	db AGILITY      ; FURRET
