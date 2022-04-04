607b0fe512f5340bfed7460951b37af4bf599ef3
U-LAPTOP-J49EUBLE\willy <willyfudgehj@gmail.com>
2022-04-04 09:21:28 -0500

    Dodaerie add WIP

---
+++ data/pokemon/base_stats/dodaerie.asm
@@ -0,0 +1,21 @@
+	db DODAERIE ; 241
+
+	db  30,  70,  40,  65,  30,  30
+	;   hp  atk  def  spd  sat  sdf
+
+	db NORMAL, FLYING ; type
+	db 190 ; catch rate
+	db 56 ; base exp
+	db NO_ITEM, SHARP_BEAK ; items
+	db GENDER_F50 ; gender ratio
+	db 100 ; unknown 1
+	db 20 ; step cycles to hatch
+	db 5 ; unknown 2
+	INCBIN "gfx/pokemon/dodaerie/front.dimensions"
+	db 0, 0, 0, 0 ; padding
+	db GROWTH_MEDIUM_FAST ; growth rate
+	dn EGG_FLYING, EGG_FLYING ; egg groups
+
+	; tm/hm learnset
+	tmhm CURSE, TOXIC, HIDDEN_POWER, SUNNY_DAY, PROTECT, ENDURE, RETURN, MUD_SLAP, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, SWIFT, REST, ATTRACT, THIEF, STEEL_WING, WIND_RIDE
+	; end
