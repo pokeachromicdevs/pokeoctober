3b74d5fa3fe74c30028024d88bf61bc69b2fe14b
U-LAPTOP-J49EUBLE\willy <willyfudgehj@gmail.com>
2022-05-07 18:08:17 -0500

    Medium in Sakura Shrine

--- maps/CherrygroveShrine.asm
+++ maps/CherrygroveShrine.asm
@@ -1,7 +1,29 @@
+	object_const_def
+	const SAKURASHRINE_MEDIUM
+	const SAKURASHRINE_SAGE
+	const SAKURASHRINE_GRAMPS
+
 CherrygroveShrine_MapScripts:
 	db 0 ; scene scripts
 
 	db 0 ; callbacks
+	
+SakuraShrineMediumScript:
+	jumptextfaceplayer SakuraShrineMediumText
+	
+SakuraShrineMediumText:
+	text "Ah, it's not every-"
+	line "day a young one such"
+	
+	para "as yourself comes to"
+	line "to the SAKURA SHRINE."
+	
+	para "It's reassuring that"
+	line "the young ones still"
+	
+	para "wish to pay respects."
+	done
+
 
 CherrygroveShrine_MapEvents:
 	db 0, 0 ; filler
@@ -14,4 +36,6 @@ CherrygroveShrine_MapEvents:
 
 	db 0 ; bg events
 
-	db 0 ; object events
+	db 1 ; object events
+	object_event  15,  9, SPRITE_GRANNY, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_BROWN, OBJECTTYPE_SCRIPT, 0, SakuraShrineMediumScript, -1
+
