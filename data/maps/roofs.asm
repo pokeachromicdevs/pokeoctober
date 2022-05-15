65d15cd29f79efd5ff4bf78b7ce0e8d83fb23e1f
Zumi Daxuya <daxuya.zumi+code@protonmail.com>
2022-05-15 15:58:33 +0700

    maps: yeet roofs until uproot trees are fixed

--- data/maps/roofs.asm
+++ data/maps/roofs.asm
@@ -10,17 +10,17 @@ MapGroupRoofs:
 ; entries correspond to map groups
 ; values are indexes for Roofs (see below)
 	db -1             ;  0
-	db ROOF_OLIVINE   ;  1 (Olivine)
-	db ROOF_AZALEA    ;  2 (Mahogany)
+	db -1   ;  1 (Olivine)
+	db -1    ;  2 (Mahogany)
 	db -1             ;  3
-	db ROOF_VIOLET    ;  4 (Ecruteak)
-	db ROOF_AZALEA    ;  5 (Blackthorn)
+	db -1    ;  4 (Ecruteak)
+	db -1    ;  5 (Blackthorn)
 	db -1             ;  6
 	db -1             ;  7
-	db ROOF_SILENT    ;  8 (Azalea)
-	db ROOF_AZALEA    ;  9 (Lake of Rage)
-	db ROOF_SILENT  ; 10 (Violet)
-	db ROOF_GOLDENROD ; 11 (Goldenrod)
+	db -1    ;  8 (Azalea)
+	db -1    ;  9 (Lake of Rage)
+	db -1  ; 10 (Violet)
+	db -1 ; 11 (Goldenrod)
 	db -1             ; 12
 	db -1             ; 13
 	db -1             ; 14
@@ -28,16 +28,15 @@ MapGroupRoofs:
 	db -1             ; 16
 	db -1             ; 17
 	db -1             ; 18
-	db ROOF_SILENT  ; 19 (Silver Cave)
+	db -1  ; 19 (Silver Cave)
 	db -1             ; 20
 	db -1             ; 21
-	db ROOF_OLIVINE   ; 22 (Cianwood)
+	db -1   ; 22 (Cianwood)
 	db -1             ; 23
-	db ROOF_SILENT  ; 24 (New Bark)
+	db -1  ; 24 (New Bark)
 	db -1             ; 25
 	db -1             ; 26 (Cherrygrove)
 	db -1             ; 27 (Citrine)
-	db -1             ; 28 (Shrine)
 Roofs:
 ; entries correspond to ROOF_* constants
 INCBIN "gfx/tilesets/roofs/new_bark.2bpp"
