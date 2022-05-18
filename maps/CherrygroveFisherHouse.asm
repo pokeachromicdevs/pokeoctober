fc2bee2ec62b201eb6bf0b186ed4ee1bf6468d7c
Zumi Daxuya <daxuya.zumi+code@protonmail.com>
2022-05-18 13:18:26 +0700

    maps/cherrygrove: fix interior warps

--- maps/CherrygroveFisherHouse.asm
+++ maps/CherrygroveFisherHouse.asm
@@ -12,8 +12,8 @@ CherrygroveFisherHouse_MapEvents:
 	db 0, 0 ; filler
 
 	db 2 ; warp events
-	warp_event  2,  7, CHERRYGROVE_CITY, 3
-	warp_event  3,  7, CHERRYGROVE_CITY, 3
+	warp_event  2,  7, CHERRYGROVE_CITY, 9
+	warp_event  3,  7, CHERRYGROVE_CITY, 9
 
 	db 0 ; coord events
 
