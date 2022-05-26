6c1b3d58be43c4a65b1355cb9036d62fbf3ee39e
Zumi Daxuya <daxuya.zumi+code@protonmail.com>
2022-05-26 22:21:37 +0700

    follower: try to spawn appropriately

--- engine/pokemon/following.asm
+++ engine/pokemon/following.asm
@@ -8,14 +8,69 @@ SetPartyNumberAsFollower::
 ; Follower flags = which number in party is following you
 	ld [wFollowerFlags], a
 
-; fix position of follow obj
-	ld b, FOLLOWER
+; kind of crude check
+	ld a, [wTileLeft]
+	call GetTileCollision
+	jr z, .spawn_left
+
+	ld a, [wTileRight]
+	call GetTileCollision
+	jr z, .spawn_right
+
+	ld a, [wTileDown]
+	call GetTileCollision
+	jr z, .spawn_down
+
+	ld a, [wTileUp]
+	call GetTileCollision
+	jr z, .spawn_up
+
+.spawn_down
+	ld a, [wXCoord]
+	add 4
+	ld d, a
+	ld a, [wYCoord]
+	add 5
+	ld e, a
+	jr .got_spawn
+
+.spawn_up
+	ld a, [wXCoord]
+	add 4
+	ld d, a
+	ld a, [wYCoord]
+	add 3
+	ld e, a
+	jr .got_spawn
+
+.spawn_left
+	ld a, [wXCoord]
+	add 3
+	ld d, a
+	ld a, [wYCoord]
+	add 4
+	ld e, a
+	jr .got_spawn
+
+.spawn_right
+	ld a, [wXCoord]
+	add 5
+	ld d, a
+	ld a, [wYCoord]
+	add 4
+	ld e, a
+	jr .got_spawn
+
+.spawn_center ; if all else fails
 	ld a, [wXCoord]
 	add 4
 	ld d, a
 	ld a, [wYCoord]
 	add 4
 	ld e, a
+
+.got_spawn
+	ld b, FOLLOWER
 	farcall CopyDECoordsToMapObject
 	farcall ChangeFollowerColor
 	ld a, FOLLOWER
