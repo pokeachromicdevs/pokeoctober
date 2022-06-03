25c1045209dede96c10facde0929894a69e44998
Zumi Daxuya <daxuya.zumi+code@protonmail.com>
2022-06-03 22:42:03 +0700

    follower: poisoned interaction
    ...before any standard interaction scripts are written, for some reason.

--- engine/events/follower.asm
+++ engine/events/follower.asm
@@ -1,9 +1,38 @@
 SECTION "Follower Script", ROMX
 
 FollowerScript::
-	jumptextfaceplayer .Text
+	callasm .IsFollowerPoisoned
+	iftrue .Poisoned
 
-.Text
+; regular script goes here
+	jumptextfaceplayer .Text
+.Text:
 	text "This"
 	done
 
+.Poisoned:
+; poisoned script
+	showemote EMOTE_SAD, FOLLOWER, 30
+	end
+
+.IsFollowerPoisoned:
+	ld a, [wCurPartyMon]
+	push af
+		ld a, [wWhichPartyFollower] ; assume this is always > 0
+		dec a
+		ld [wCurPartyMon], a
+		ld a, MON_STATUS
+		call GetPartyParamLocation
+		ld a, [hl]
+		and 1 << PSN
+		jr z, .not_poisoned
+		ld a, 1 ; poisoned
+		jr .done_checking
+.not_poisoned
+		xor a ; not poisoned
+		jr .done_checking
+.done_checking
+		ld [wScriptVar], a
+	pop af
+	ld [wCurPartyMon], a
+	ret
