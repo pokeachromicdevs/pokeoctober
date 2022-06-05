a22a2261207cea0575692df3840ac3b1077c2ad0
cybershellxii <cybershellxii@aol.com>
2022-06-05 08:23:19 -0400

    add adbarstork v2 animation

--- gfx/pokemon/adbarstork/anim.asm
+++ gfx/pokemon/adbarstork/anim.asm
@@ -1,16 +1,16 @@
-	frame 0, 10
-	frame 1, 05
-	frame 2, 24
-	frame 1, 05
-	frame 2, 16
+	frame 0, 07
+	frame 1, 06
+	frame 2, 18
+	frame 1, 06
+	frame 2, 10
 	frame 1, 04
-	frame 3, 08
-	frame 4, 08
-	frame 3, 08
-	frame 4, 08
-	frame 3, 08
-	frame 5, 10
-	frame 6, 10
+	frame 3, 07
+	frame 4, 07
+	frame 3, 07
+	frame 4, 07
+	frame 3, 05
+	frame 5, 07
+	frame 6, 04
 	frame 7, 07
 	frame 8, 07
 	frame 7, 07
@@ -18,8 +18,6 @@
 	frame 7, 07
 	frame 8, 07
 	frame 7, 07
-	frame 9, 08
-	frame 0, 08
+	frame 9, 04
 	endanim
 	
-;check this animation later to see if it fully matches the .gif
