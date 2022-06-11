09917f82833be7efaa6e910d36e4db8fe7a8e7b5
Zumi Daxuya <daxuya.zumi+code@protonmail.com>
2022-06-11 07:41:30 +0700

    follower: custom starter messages

--- engine/events/follower.asm
+++ engine/events/follower.asm
@@ -10,34 +10,16 @@ FollowerScript::
 
 ; poisoned script takes priority
 	callasm .IsFollowerPoisoned
-	iftrue .Poisoned
+	iftrue FollowerPoisonedScript
 
-; regular script goes here
-	random 5
-	ifequal  0, FollowerScriptChoice1
-	ifequal  1, FollowerScriptChoice2
-	ifequal  2, FollowerScriptChoice3
-	ifequal  3, FollowerScriptChoice4
-	ifequal  4, FollowerScriptChoice5
+; ...then starter mons
+	callasm .GetFollowerSpecies
+	ifequal16 CHIKORITA, FollowerChikoritaScript
+	ifequal16 CYNDAQUIL, FollowerCyndaquilScript
+	ifequal16  TOTODILE, FollowerTotodileScript
 
-; invalid or i somehow missed one
-	jumptextfaceplayer .Text
-.Text:
-	text "This"
-	done
-
-.Poisoned:
-; poisoned script
-	showemote EMOTE_SAD, FOLLOWER, 30
-	jumptext .PoisonedText
-	end
-
-.PoisonedText
-	text_ram wStringBuffer1
-	text " is"
-	line "shivering from the"
-	cont "poison<...>"
-	done
+; ...then fallback to generic script
+	sjump FollowerGenericScript
 
 .GetFollowerNickname:
 	ld a, [wWhichPartyFollower]
@@ -46,6 +28,16 @@ FollowerScript::
 	dec a
 	ld hl, wPartyMonNicknames	
 	jp GetNick
+
+.GetFollowerSpecies:
+	push hl
+		call GetFollowingPokemon
+		ld a, l
+		ld [wScriptWordVar], a
+		ld a, h
+		ld [wScriptWordVar + 1], a
+	pop hl
+	ret
 
 .IsFollowerPoisoned:
 	ld a, [wCurPartyMon]
@@ -69,7 +61,156 @@ FollowerScript::
 	ld [wCurPartyMon], a
 	ret
 
-; randomized choice
+
+FollowerChikoritaScript:
+	random 2
+	iftrue FollowerGenericScript
+	random 3
+	ifequal 0, FollowerChikoritaScriptChoice1
+	ifequal 1, FollowerChikoritaScriptChoice2
+	ifequal 2, FollowerChikoritaScriptChoice3
+	sjump FollowerGenericScript ; fallback
+
+FollowerChikoritaScriptChoice1:
+	jumptext .Text
+.Text:
+	text_ram wStringBuffer1
+	text "'s leaf"
+	line "is emitting a"
+	cont "lovely scent."
+	done
+
+FollowerChikoritaScriptChoice2:
+	jumptext .Text
+.Text:
+	text_ram wStringBuffer1
+	text " is"
+	line "waving its leaf"
+	cont "around happily!"
+	done
+
+FollowerChikoritaScriptChoice3:
+	jumptext .Text
+.Text:
+	text_ram wStringBuffer1
+	text " is"
+	line "using its vines to"
+	cont "feel around its"
+	cont "surroundings."
+	done
+
+FollowerCyndaquilScript:
+	random 2
+	iftrue FollowerGenericScript
+	readmem wEnvironment
+	ifequal INDOOR, .indoors
+; outdoors or caves or routes etc.
+	random 3
+	ifequal 0, FollowerCyndaquilScriptChoice1
+	ifequal 1, FollowerCyndaquilScriptChoice2
+	ifequal 2, FollowerCyndaquilScriptChoiceBerry
+	sjump FollowerGenericScript ; fallback
+
+.indoors
+	random 2
+	ifequal 0, FollowerCyndaquilScriptChoice1
+	ifequal 1, FollowerCyndaquilScriptChoice2
+	sjump FollowerGenericScript ; fallback
+
+FollowerCyndaquilScriptChoice1:
+	jumptext .Text
+.Text:
+	text_ram wStringBuffer1
+	text " flared"
+	line "the flames on its"
+	cont "back!"
+	done
+
+FollowerCyndaquilScriptChoice2:
+	jumptext .Text
+.Text:
+	text_ram wStringBuffer1
+	text "'s flames"
+	line "appear bright red!"
+	done
+
+FollowerCyndaquilScriptChoiceBerry:
+	opentext
+	writetext .Text
+	yesorno
+	iffalse .done
+	verbosegiveitem BURNT_BERRY
+.done
+	closetext
+	end
+.Text:
+	text_ram wStringBuffer1
+	text " found a"
+	line "BERRY and is"
+	cont "roasting it."
+
+	para "Want it?"
+	done
+
+FollowerTotodileScript:
+	random 2
+	iftrue FollowerGenericScript
+	random 3
+	ifequal 0, FollowerTotodileScriptChoice1
+	ifequal 1, FollowerTotodileScriptChoice2
+	ifequal 2, FollowerTotodileScriptChoice3
+	sjump FollowerGenericScript ; fallback
+
+FollowerTotodileScriptChoice1:
+	jumptext .Text
+.Text:
+	text_ram wStringBuffer1
+	text " chomps"
+	line "you lightly."
+	done
+
+FollowerTotodileScriptChoice2:
+	jumptext .Text
+.Text:
+	text_ram wStringBuffer1
+	text_start
+	line "squirted you with"
+	cont "water!"
+	done
+
+FollowerTotodileScriptChoice3:
+	jumptext .Text
+.Text:
+	text_ram wStringBuffer1
+	text " lets a"
+	line "loose cry of ex-"
+	cont "citement."
+	done
+
+FollowerPoisonedScript:
+; poisoned script
+	showemote EMOTE_SAD, FOLLOWER, 30
+	jumptext .Text
+	end
+.Text:
+	text_ram wStringBuffer1
+	text " is"
+	line "shivering from the"
+	cont "poison<...>"
+	done
+
+FollowerGenericScript:
+	random 5
+	ifequal  0, FollowerScriptChoice1
+	ifequal  1, FollowerScriptChoice2
+	ifequal  2, FollowerScriptChoice3
+	ifequal  3, FollowerScriptChoice4
+	ifequal  4, FollowerScriptChoice5
+; invalid or i somehow missed one
+	jumptext .InvalidText
+.InvalidText:
+	text "This"
+	done
 
 FollowerScriptChoice1:
 	jumptext .Text
