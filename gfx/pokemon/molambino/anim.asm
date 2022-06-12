84a6f31c447046d71b392fd56ebacb9527dda021
cybershellxii <cybershellxii@aol.com>
2022-06-12 18:43:08 -0400

    add molambino animation

--- gfx/pokemon/molambino/anim.asm
+++ gfx/pokemon/molambino/anim.asm
@@ -1,1 +1,20 @@
+	frame 0, 06
+	frame 1, 04
+	frame 2, 06
+	frame 3, 06
+	frame 4, 06
+	frame 5, 06
+	frame 6, 06
+	frame 7, 06
+	frame 8, 06
+	frame 9, 06
+	setrepeat 1
+	frame 10, 05
+	frame 11, 05
+	frame 12, 05
+	frame 13, 05
+	frame 14, 05
+	frame 15, 05
+	dorepeat 11
+	frame 1, 05
 	endanim
--- gfx/pokemon/molambino/anim_idle.asm
+++ gfx/pokemon/molambino/anim_idle.asm
@@ -1,1 +1,10 @@
+	frame 0, 06
+	frame 1, 04
+	frame 10, 05
+	frame 11, 05
+	frame 12, 05
+	frame 13, 05
+	frame 14, 05
+	frame 15, 05
+	frame 1, 04
 	endanim
