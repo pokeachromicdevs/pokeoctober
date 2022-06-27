173d8c69ac909fadf92ffe26f855f7279397de73
Zumi Daxuya <daxuya.zumi+code@protonmail.com>
2022-06-27 10:22:32 +0700

    maps/alderpokecenter1f: add gameboy guy

--- maps/AlderPokecenter1F.asm
+++ maps/AlderPokecenter1F.asm
@@ -13,6 +13,27 @@ AlderPokecenter1F_MapScripts:
 AlderPokecenter1FNurseScript:
 	jumpstd pokecenternurse
 
+AlderPokecenter1FGameBoyGuyScript:
+	jumptextfaceplayer .Txt
+
+.Txt:
+	text "I heard that"
+	line "trainers across"
+	para "the pond are able"
+	line "to connect with"
+	para "one another around"
+	line "the world with"
+	para "their GAME BOY"
+	line "COLOR."
+	para "Unfortunately, we"
+	line "don't have that"
+	para "sort of tech-"
+	line "nology here."
+	para "Sigh<...> if only"
+	line "things were more"
+	cont "advanced."
+	done
+
 AlderPokecenter1F_MapEvents:
 	db 0, 0 ; filler
 
@@ -25,6 +46,7 @@ AlderPokecenter1F_MapEvents:
 
 	db 0 ; bg events
 
-	db 2 ; object events
+	db 3 ; object events
 	object_event  5,  1, SPRITE_NURSE, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, AlderPokecenter1FNurseScript, -1
 	object_event  9,  5, SPRITE_KURT, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, ObjectEvent, EVENT_KURT_IN_ALDER_POKEMON_CENTER
+	object_event 10,  5, SPRITE_GAMEBOY_KID, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, AlderPokecenter1FGameBoyGuyScript, -1
