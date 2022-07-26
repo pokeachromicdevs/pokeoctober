f09258177e16fb19ae5c7677caa3d7fe3e974621
Zumi Daxuya <daxuya.zumi+code@protonmail.com>
2022-07-26 23:36:32 +0700

    pokemon/wooper: finally some PROPER animation

--- gfx/pokemon/wooper/anim.asm
+++ gfx/pokemon/wooper/anim.asm
@@ -1,7 +1,17 @@
-	setrepeat 2
-	frame 1, 08
-	frame 2, 08
-	dorepeat 1
-	frame 0, 08
-	frame 4, 08
+	frame  0, 06
+	frame  1, 18
+	frame  2, 06
+	frame  1, 06
+	frame  2, 06
+	frame  1, 06
+	frame  3, 06
+	frame  4, 24
+	frame  5, 06
+	frame  6, 06
+	frame  7, 06
+	frame  2, 06
+	frame  8, 06
+	frame  9, 06
+	frame 10, 06
+	frame  2, 06
 	endanim
