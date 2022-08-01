bba3a30e5340b6487db8d2bff419834b0a703b76
cybershellxii <cybershellxii@aol.com>
2022-08-01 16:00:52 -0400

    add pupperon animation

--- gfx/pokemon/pupperon/anim.asm
+++ gfx/pokemon/pupperon/anim.asm
@@ -1,2 +1,19 @@
-	frame 0, 08
+	frame  0, 06
+	setrepeat 3
+	frame  1, 07
+	frame  2, 07
+	dorepeat 2
+	frame  3, 06
+	frame  4, 07
+	frame  5, 09
+	frame  4, 06
+	frame  5, 06
+	frame  6, 12
+	frame  5, 06
+	frame  4, 06
+	frame  5, 06
+	frame  7, 03
+	frame  8, 03
+	frame  9, 06
+	frame  3, 06
 	endanim
