31c5a370e7bf3d813120d42e78f4bccf1c5ad30e
cybershellxii <cybershellxii@aol.com>
2022-08-01 15:54:35 -0400

    add vulpiii animations

--- gfx/pokemon/vulpiii/anim.asm
+++ gfx/pokemon/vulpiii/anim.asm
@@ -1,1 +1,18 @@
+	frame  0, 06
+	frame  1, 06
+	frame  2, 06
+	frame  3, 06
+	frame  1, 06
+	frame  2, 06
+	setrepeat 1
+	frame  4, 05
+	frame  5, 05
+	frame  6, 05
+	frame  7, 05
+	dorepeat 7
+	frame  3, 06
+	frame  8, 12
+	frame  9, 06
+	frame  8, 12
+	frame  3, 06
 	endanim
--- gfx/pokemon/vulpiii/anim_idle.asm
+++ gfx/pokemon/vulpiii/anim_idle.asm
@@ -1,1 +1,7 @@
+	frame  0, 06
+	frame  7, 05
+	frame  8, 12
+	frame  7, 05
+	frame  8, 12
+	frame  9, 06
 	endanim
