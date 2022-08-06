c7fcb4d5e4b4536ed8908cb5337150d31892c16d
Zumi Daxuya <daxuya.zumi+code@protonmail.com>
2022-08-06 19:55:31 +0700

    maps/azaleamart: npc's

--- maps/AzaleaMart.asm
+++ maps/AzaleaMart.asm
@@ -12,6 +12,43 @@ AzaleaMartClerkScript:
 	closetext
 	end
 
+AzaleaMart_TwinScript:
+	jumptextfaceplayer .Text
+.Text:
+	text "FLOWER MAIL is so"
+	line "pretty!"
+	para "Orange is even my"
+	line "favorite color!"
+	done
+
+AzaleaMart_BlackbeltScript:
+	jumptextfaceplayer .Text
+.Text:
+	text "CHARCOAL slightly"
+	line "increases the"
+	para "power of fire-type"
+	line "moves."
+	para "My FARFETCH'D"
+	line "helps me find"
+	para "twigs to use in"
+	line "order to make it."
+	done
+
+AzaleaMart_GrannyScript:
+	jumptextfaceplayer .Text
+.Text:
+	text "SUPER POTION is"
+	line "slightly stronger"
+	cont "than POTION."
+	para "While a POTION"
+	line "heals 20 HP, a"
+	para "SUPER POTION heals"
+	line "50 HP."
+	para "However, they are"
+	line "more expensive so"
+	cont "choose wisely."
+	done
+
 AzaleaMart_MapEvents:
 	db 0, 0 ; filler
 
@@ -23,6 +60,8 @@ AzaleaMart_MapEvents:
 
 	db 0 ; bg events
 
-	db 1 ; object events
+	db 4 ; object events
 	object_event  1,  3, SPRITE_CLERK, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, AzaleaMartClerkScript, -1
-
+	object_event 11,  3, SPRITE_TWIN, SPRITEMOVEDATA_WALK_LEFT_RIGHT, 1, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, AzaleaMart_TwinScript, -1
+	object_event  7,  6, SPRITE_BLACK_BELT, SPRITEMOVEDATA_WALK_LEFT_RIGHT, 1, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, AzaleaMart_BlackbeltScript, -1
+	object_event  4,  1, SPRITE_GRANNY, SPRITEMOVEDATA_WALK_LEFT_RIGHT, 1, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, AzaleaMart_GrannyScript, -1
