2cebd5f174e5caeedfd738dad148630ae64272da
cybershellxii <cybershellxii@aol.com>
2022-08-18 13:47:41 -0400

    add coalta animations

--- gfx/pokemon/coalta/anim.asm
+++ gfx/pokemon/coalta/anim.asm
@@ -1,1 +1,20 @@
+	frame  0, 07
+	frame  1, 07
+	frame  2, 07
+	frame  3, 07
+	frame  1, 07
+	frame  4, 07
+	frame  5, 07
+	frame  6, 07
+	frame  7, 07
+	frame  8, 07
+	frame  9, 07
+	frame 10, 07
+	setrepeat 1
+	frame 11, 07
+	frame 12, 01
+	frame 13, 05
+	frame 14, 07
+	dorepeat 13
+	frame 15, 07
 	endanim
--- gfx/pokemon/coalta/anim_idle.asm
+++ gfx/pokemon/coalta/anim_idle.asm
@@ -1,1 +1,8 @@
+    frame  0, 07
+	setrepeat 1
+	frame  1, 07
+	frame  2, 07
+	frame  3, 07
+	dorepeat 2
+	frame  1, 07
 	endanim
