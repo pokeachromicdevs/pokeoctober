e5cb3ad1c1db174cafd2444fef9b25df80fcbea6
Zumi Daxuya <daxuya.zumi+code@protonmail.com>
2022-08-20 11:07:40 +0700

    maps/azalea: residence

--- maps/AzaleaLeftHouse.asm
+++ maps/AzaleaLeftHouse.asm
@@ -8,6 +8,84 @@ AzaleaLeftHouse_MapScripts:
 AzaleaLeftHouseBookshelf:
 	jumpstd picturebookshelf
 
+AzaleaLeftHouse_CoolF:
+	jumptextfaceplayer .Text
+.Text:
+	text "My daughter really"
+	line "looks after her"
+	cont "grandma."
+	para "She specializes in"
+	line "ghost-type #MON"
+	para "and is a very res-"
+	line "pected trainer."
+	done
+
+AzaleaLeftHouse_CoolM:
+	faceplayer
+	opentext
+	writetext .Text1
+	yesorno
+	iftrue .yes
+	writetext .Text2
+	waitbutton
+	closetext
+	end
+.yes
+	writetext .Text3
+	waitbutton
+	closetext
+	end
+.Text1:
+	text "Mother was always"
+	line "energetic, and she"
+	para "wanted to fight"
+	line "with #MON every"
+	cont "day."
+	para "Would you like to"
+	line "know how she met"
+	cont "PROF. OAK?"
+	done
+.Text2:
+	text "That's quite"
+	line "alright! I'll tell"
+	para "you some other"
+	line "time!"
+	para "Don't worry. I'll"
+	line "always be here."
+	done
+.Text3:
+	text "When mother was"
+	line "just a young girl,"
+	para "she loved to play"
+	line "and romp around."
+	para "She would chall-"
+	line "enge anyone with"
+	para "her most trusted"
+	line "#MON, a GASTLY"
+	para "that she found"
+	line "while visiting the"
+	para "#MON TOWER in"
+	line "KANTO."
+	para "That was when she"
+	line "noticed OAK, who"
+	para "always had her in"
+	line "a good book."
+	para "Despite this, she'd"
+	line "challenge him, and"
+	cont "always lose."
+	para "Wondering why, she"
+	line "dedicated her life"
+	cont "to battling."
+	para "This is why she"
+	line "got so angry when"
+	para "OAK announced"
+	line "that he was"
+	para "developing the"
+	line "#DEX, because"
+	para "she thought he was"
+	line "wasting his"
+	cont "potential."
+	done
 
 AzaleaLeftHouse_MapEvents:
 	db 0, 0 ; filler
@@ -22,5 +100,6 @@ AzaleaLeftHouse_MapEvents:
 	bg_event  0,  1, BGEVENT_READ, AzaleaLeftHouseBookshelf
 	bg_event  1,  1, BGEVENT_READ, AzaleaLeftHouseBookshelf
 
-	db 0 ; object events
-	;object_event  2,  4, SPRITE_FISHER, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, 20, 6, PAL_NPC_GREEN, OBJECTTYPE_SCRIPT, 0, MysticWaterGuy_2, -1
+	db 2 ; object events
+	object_event  2,  4, SPRITE_COOLTRAINER_M, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, -1, PAL_NPC_RED, OBJECTTYPE_SCRIPT, 0, AzaleaLeftHouse_CoolM, -1
+	object_event  5,  4, SPRITE_COOLTRAINER_F, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_SCRIPT, 0, AzaleaLeftHouse_CoolF, -1
--- maps/AzaleaRightHouse.asm
+++ maps/AzaleaRightHouse.asm
@@ -8,6 +8,28 @@ AzaleaRightHouse_MapScripts:
 AzaleaRightHouseBookshelf:
 	jumpstd picturebookshelf
 
+AzaleaRightHouse_Sage:
+	jumptextfaceplayer .Text
+.Text:
+	text "My FENNECURSE"
+	line "keeps guard while"
+	para "I'm sleeping or out"
+	line "in the ZEN GARDEN."
+	para "It's been my part-"
+	line "ner for years now."
+	done
+
+AzaleaRightHouse_Foxxo:
+	opentext
+	writetext .Text
+	cry FENNECURSE
+	waitsfx
+	waitbutton
+	closetext
+	end
+.Text:
+	text "FENNECURSE: Skree!"
+	done
 
 AzaleaRightHouse_MapEvents:
 	db 0, 0 ; filler
@@ -22,5 +44,6 @@ AzaleaRightHouse_MapEvents:
 	bg_event  0,  1, BGEVENT_READ, AzaleaRightHouseBookshelf
 	bg_event  1,  1, BGEVENT_READ, AzaleaRightHouseBookshelf
 
-	db 0 ; object events
-	;object_event  2,  4, SPRITE_FISHER, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, 20, 6, PAL_NPC_GREEN, OBJECTTYPE_SCRIPT, 0, MysticWaterGuy_2, -1
+	db 2 ; object events
+	object_event  2,  4, SPRITE_SAGE, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, -1, PAL_NPC_BROWN, OBJECTTYPE_SCRIPT, 0, AzaleaRightHouse_Sage, -1
+	object_event  6,  3, SPRITE_GROWLITHE, SPRITEMOVEDATA_STANDING_DOWN, 2, 2, -1, -1, PAL_NPC_RED, OBJECTTYPE_SCRIPT, 0, AzaleaRightHouse_Foxxo, -1
