4fb26df2295a039516bf66618e0e9d98879bc9f8
Zumi Daxuya <daxuya.zumi+code@protonmail.com>
2022-08-21 20:10:44 +0700

    items: 16-bit, part 1
    set up for 16 bit item tomfoolery

--- constants/16_bit_locking_constants.asm
+++ constants/16_bit_locking_constants.asm
@@ -39,3 +39,9 @@ endc
 if const_value > MOVE_TABLE_LOCKED_ENTRIES
 	fail "Too many locked move IDs"
 endc
+
+; Item locked ID table constants
+	const_def
+if const_value > ITEM_TABLE_LOCKED_ENTRIES
+	fail "Too many locked item IDs"
+endc
