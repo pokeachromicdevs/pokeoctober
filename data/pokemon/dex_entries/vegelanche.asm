c2a4ba746fef38cc09a8da213a500169d8bda5d7
cybershellxii <cybershellxii@aol.com>
2022-09-03 15:47:55 -0400

    properly add vegelanche

---
+++ data/pokemon/dex_entries/vegelanche.asm
@@ -0,0 +1,9 @@
+	db "SNOWMAN@" ; species name
+	dw 409, 4113 ; height, weight
+
+	db   "It digs deep into"
+	next "snow mounds in"
+	next "search of its"
+
+	page "favorite food,"
+	next "spring grass.@"
---
+++ gfx/pokemon/vegelanche/anim.asm
@@ -0,0 +1,16 @@
+	frame  0, 07
+	frame  1, 07
+	frame  2, 07
+	frame  1, 07
+	frame  3, 07
+	frame  4, 10
+	frame  5, 07
+	frame  2, 07
+	frame  3, 07
+	frame  4, 10
+	frame  5, 07
+	frame  3, 07
+	frame  4, 10
+	frame  5, 07
+	frame  2, 07
+	endanim
