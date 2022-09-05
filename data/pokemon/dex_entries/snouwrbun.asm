aaf176f4683dbf24d5943b57dd8a8423339f470c
cybershellxii <cybershellxii@aol.com>
2022-09-05 19:06:09 -0400

    properly add snouwrbun

---
+++ data/pokemon/dex_entries/snouwrbun.asm
@@ -0,0 +1,10 @@
+	db "SOUR@" ; species name
+	dw 410, 4045 ; height, weight
+
+	db   "It is constantly"
+	next "in a sour mood"
+	next "in the day but"
+
+	page "at night it plays"
+	next "with other members"
+	next "of its species.@"
---
+++ gfx/pokemon/snouwrbun/anim.asm
@@ -0,0 +1,14 @@
+	frame  0, 09
+	frame  1, 09
+	frame  2, 07
+	frame  3, 09
+	frame  4, 09
+	frame  5, 09
+	frame  6, 09
+	frame  7, 07
+	frame  8, 07
+	frame  9, 07
+	frame 10, 07
+	frame  2, 07
+	frame  1, 09
+	endanim
