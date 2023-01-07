2d5a8ed65511bd096d83b72edf0d13152bb24b0a
Zumi Daxuya <daxuya.zumi+code@protonmail.com>
2023-01-07 16:13:33 +0700

    pokemon/heracross,murkrow: use original animation

--- gfx/pokemon/heracross/anim.asm
+++ gfx/pokemon/heracross/anim.asm
@@ -4,13 +4,15 @@
 	frame  1, 04
 	frame  3, 04
 	frame  4, 04
-	frame  5, 09
+	frame  5, 04
 	frame  6, 04
 	frame  7, 04
-	frame  8, 06
-	frame  9, 06
 	frame  8, 04
-	frame  9, 12
+	frame  9, 06
+	frame 10, 06
+	frame  9, 04
+	frame 10, 07
+	frame  6, 04
 	frame  5, 04
 	frame  2, 04
 	frame  3, 06
--- gfx/pokemon/murkrow/anim.asm
+++ gfx/pokemon/murkrow/anim.asm
@@ -1,16 +1,22 @@
 	frame  0, 06
-	frame  1, 12
+	frame  1, 06
 	frame  2, 06
 	frame  3, 06
 	frame  4, 06
-	frame  3, 06
-	frame  2, 06
-	frame  3, 12
 	frame  5, 06
-	frame  6, 18
-	frame  5, 12
+	frame  4, 06
 	frame  3, 06
-	frame  4, 12
-	frame  1, 06
+	frame  4, 06
+	frame  6, 06
 	frame  7, 06
+	frame  8, 06
+	frame  9, 06
+	frame  8, 06
+	frame  7, 06
+	frame  6, 06
+	frame  4, 06
+	frame  5, 06
+	frame  2, 06
+	frame  1, 06
+	frame 10, 06
 	endanim
