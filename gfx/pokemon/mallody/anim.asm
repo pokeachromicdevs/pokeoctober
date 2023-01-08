ddd8303a23bf8295e2f8a6c53af047df225b3ab8
cybershellxii <cybershellxii@aol.com>
2023-01-08 06:24:11 -0500

    add clefling and mallody animations

--- gfx/pokemon/mallody/anim.asm
+++ gfx/pokemon/mallody/anim.asm
@@ -1,1 +1,15 @@
+	frame  0, 18
+	frame  1, 18
+	frame  2, 09
+	frame  3, 19
+	frame  4, 09
+	frame  5, 09
+	frame  6, 09
+	frame  7, 09
+	frame  8, 09
+	frame  9, 09
+	frame 10, 09
+	frame 11, 09
+	frame  1, 09
+	frame 12, 18
 	endanim
