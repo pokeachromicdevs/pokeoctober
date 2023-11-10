b3a2517cf9a1383371483177089f60b2939444aa
GabenNieNie2023 <willyfudgehj@gmail.com>
2023-11-10 04:24:24 -0600

    fix Qwilfish oversight

--- gfx/pokemon/qwilfish/anim.asm
+++ gfx/pokemon/qwilfish/anim.asm
@@ -1,18 +1,6 @@
-	frame  0, 08
-	frame  1, 08
-	frame  2, 08
-	frame  1, 08
-	frame  3, 04
-	frame  4, 08
-	frame  3, 04
-	frame  5, 08
-	frame  6, 08
-	frame  7, 08
-	frame  8, 04
-	frame  9, 11
-	frame  8, 08
-	frame  9, 11
-	frame  8, 08
-	frame  6, 08
-	frame  5, 08
-	endanim
+	frame 1, 05
+	frame 2, 20
+	setrepeat 2
+	frame 0, 11
+	frame 3, 11
+	dorepeat 3
