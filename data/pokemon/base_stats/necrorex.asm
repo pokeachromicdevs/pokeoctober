67822a05bd3ddfdf0c1eeb4ec9e370de05a21eb8
root <root@GabenNieNie2023>
2023-12-05 15:54:01 -0600

    some teams and Necrorex gets real stats

--- data/pokemon/base_stats/necrorex.asm
+++ data/pokemon/base_stats/necrorex.asm
@@ -1,22 +1,21 @@
-; TODO: this is just a carbon copy of cubone's
 	db 0 ; species ID placeholder
 
-	db  50,  50,  95,  35,  40,  50
+	db  80,  105,  80, 80,  90,  80
 	;   hp  atk  def  spd  sat  sdf
 
-	db GROUND, GROUND ; type
-	db 190 ; catch rate
-	db 87 ; base exp
+	db ROCK, DRAGON ; type
+	db 45 ; catch rate
+	db 202 ; base exp
 	dw NO_ITEM, THICK_CLUB ; items
-	db GENDER_F50 ; gender ratio
+	db GENDER_F12_5 ; gender ratio
 	db 100 ; unknown 1
-	db 20 ; step cycles to hatch
+	db 35 ; step cycles to hatch
 	db 5 ; unknown 2
 	INCBIN "gfx/pokemon/necrorex/front.dimensions"
 	db 0, 0, 0, 0 ; padding
-	db GROWTH_MEDIUM_FAST ; growth rate
-	dn EGG_MONSTER, EGG_MONSTER ; egg groups
+	db GROWTH_SLOW ; growth rate
+	dn EGG_MINERAL, EGG_MONSTER ; egg groups
 
 	; tm/hm learnset
-	tmhm DYNAMICPUNCH, HEADBUTT, CURSE, TOXIC, ROCK_SMASH, HIDDEN_POWER, SUNNY_DAY, SNORE, BLIZZARD, ICY_WIND, PROTECT, ENDURE, FRUSTRATION, IRON_TAIL, EARTHQUAKE, RETURN, DIG, MUD_SLAP, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, SANDSTORM, FIRE_BLAST, THUNDERPUNCH, DETECT, REST, ATTRACT, THIEF, FIRE_PUNCH, STRONG_ARM, FLAMETHROWER, ICE_BEAM
+	tmhm DYNAMICPUNCH, HEADBUTT, CURSE, TOXIC, ROCK_SMASH, HIDDEN_POWER, SUNNY_DAY, SNORE, BLIZZARD, ICY_WIND, PROTECT, ENDURE, FRUSTRATION, IRON_TAIL, EARTHQUAKE, RETURN, DIG, MUD_SLAP, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, SANDSTORM, FIRE_BLAST, THUNDERPUNCH, DETECT, REST, ATTRACT, THIEF, FIRE_PUNCH, STRONG_ARM
 	; end
