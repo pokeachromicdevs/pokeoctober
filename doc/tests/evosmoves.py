8ce0937d6b5da9e0a88aaf17da315ec5aa53e14b
Zumi Daxuya <daxuya.zumi+code@protonmail.com>
2024-01-03 09:47:40 -0600

    docs: "evolves from" field

--- doc/tests/evosmoves.py
+++ doc/tests/evosmoves.py
@@ -6,10 +6,6 @@ import pprint
 evosmoves_db = {}
 # { ? : {"evos": ?, "moves": ?, "evolves from": ?} }
 
-# TODO: use a filter() function to find out
-# the X in "<Species> evolves from X", add it to
-# the "evolves from" key
-
 for fname in [
     "../../data/pokemon/evos_attacks_kanto.asm",
     "../../data/pokemon/evos_attacks_johto.asm",
@@ -43,7 +39,9 @@ for fname in [
                     if re.match(r"^db\s+0.+$", extract): break
                     evosmoves["evos"].append(
                         tuple(
-                            extract.replace("dbbw ", "")
+                            extract
+                            .replace("dbbw ", "")
+                            .replace("dbbbw ", "")
                             .split(", ")
                         )
                     )
@@ -65,5 +63,23 @@ for fname in [
             
             evosmoves_db[monname] = evosmoves
 
+# reflection on evos → find "evolves from"
+
+def find_evolves_from(string):
+    def _find_evolves_from(item):
+        has = False
+        evos = item[1].get("evos")
+        for i in evos:
+            has = has or (i[2] == string.upper())
+            if len(i) == 4:
+                has = has or (i[3] == string.upper())
+        return has
+    return _find_evolves_from
+
+for k, v in evosmoves_db.items():
+    for k2, v2 in filter(
+        find_evolves_from(k), evosmoves_db.items()
+    ):
+        evosmoves_db[k]['evolves from'].append(k2)
+    
 pprint.pprint(evosmoves_db)
-            
