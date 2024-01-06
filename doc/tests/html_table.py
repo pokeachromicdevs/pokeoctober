1fa8a4bd939c984b67ab941d5e27a1fd50560e9d
Zumi Daxuya <daxuya.zumi+code@protonmail.com>
2024-01-06 11:32:37 -0600

    docs: test jinja2 for html templating

--- doc/tests/html_table.py
+++ doc/tests/html_table.py
@@ -4,38 +4,23 @@
 
 import sys
 import os
+
+_dirname_ = os.path.dirname(os.path.realpath(__file__))
+
 sys.path.append(
-    os.path.dirname(
-        os.path.dirname(os.path.realpath(__file__))
-    )
+    os.path.dirname(_dirname_)
 )
 
 from subtools.get_pkmn_data import get_pokemon_data
+import jinja2
 import pprint
 
 if __name__ == "__main__":
-    for pkmn_name, pkmn_data in get_pokemon_data().items():
-        # pprint.pprint(pkmn_data)
-        print(f"""
-        <h2>{pkmn_name}</h2>
-        <h3>Base Stats</h3>
-        <table>
-            <tr>
-                <th>HP</th> <th>ATK</th>
-                <th>DEF</th> <th>SPD</th>
-                <th>SP. ATK</th> <th>SP. DEF</th>
-            </tr>
-            <tr>
-                <td>{pkmn_data["base stats"]["hp"]}</td>
-                <td>{pkmn_data["base stats"]["atk"]}</td>
-                <td>{pkmn_data["base stats"]["def"]}</td>
-                <td>{pkmn_data["base stats"]["spd"]}</td>
-                <td>{pkmn_data["base stats"]["special atk"]}</td>
-                <td>{pkmn_data["base stats"]["special def"]}</td>
-            </tr>
-        </table>
-        <h3>Can be found in</h3>
-        <ul>
-            {' '.join(["<li>%s</li>" % i for i in pkmn_data["maps"]])}
-        </ul>
-        """)
+    env = jinja2.Environment(
+        loader=jinja2.FileSystemLoader(
+            searchpath=os.path.join(_dirname_, "html")
+        )
+    )
+    with open(os.path.join(_dirname_, "html", "template.html"), "r") as tplfile:
+        template = env.from_string(tplfile.read())
+        print(template.render(data=get_pokemon_data()))
