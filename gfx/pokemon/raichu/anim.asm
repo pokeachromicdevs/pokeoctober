f9bcc12020541dd5f89393072823e19451b8cdcc
willy <willyfudgehj@gmail.com>
2025-08-29 20:53:31 -0500

    new Raichu

--- gfx/pokemon/raichu/anim.asm
+++ gfx/pokemon/raichu/anim.asm
@@ -4,8 +4,8 @@
 	frame 3, 02
 	dorepeat 2
 	setrepeat 2
-	frame 2, 03
-	frame 3, 02
+	frame 4, 05
+	frame 5, 05
 	dorepeat 6
 	frame 1, 08
 	endanim
--- gfx/pokemon/raichu/anim_idle.asm
+++ gfx/pokemon/raichu/anim_idle.asm
@@ -1,5 +1,3 @@
-	frame 0, 06
-	frame 4, 06
-	frame 0, 04
-	frame 4, 04
+	frame 0, 60
+	frame 1, 35
 	endanim
