bd2780a876966a41bc24d999fb59becfbfb4454d
Zumi Daxuya <daxuya.zumi+code@protonmail.com>
2026-03-10 07:01:57 +0700

    maps: fix stray character in map name signs
    we have some ROM0 space again so uncomment this line

--- home/text.asm
+++ home/text.asm
@@ -239,7 +239,7 @@ ENDM
 	dict "<PKMN>",    PlacePKMN
 	dict "<POKE>",    PlacePOKE
 	dict "%",         NextChar
-	;dict "¯",         " "
+	dict "¯",         " "
 	dict "<DEXEND>",  PlaceDexEnd
 	dict "<TARGET>",  PlaceMoveTargetsName
 	dict "<USER>",    PlaceMoveUsersName
